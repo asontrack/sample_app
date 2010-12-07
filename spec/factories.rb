0095b21b4bf5ef825700900ca24386278c93ded8
Ason <asontrack@gmail.com>
2010-12-07 21:05:35 +0100

    improve user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   'ason track'
+  user.email                  'asontrack@gmail.com'
+  user.password               'foobar'
+  user.password_confirmation  'foobar'
+end
